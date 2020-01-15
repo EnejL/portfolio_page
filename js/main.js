2a00dec7807435bcca974726897044d5dd131946
enejl <enej.licina@riffonline.com>
2020-01-15 11:34:40 +0100

    finesse

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// nav toggle js
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
@@ -9,4 +11,27 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     });
+});
+
+document.addEventListener('keydown', (e) => {
+    if(e.keyCode == 27) {
+        document.body.classList.remove('nav-open');
+    }
+});
+
+// scroll to top js
+
+const toTop = document.querySelector('.scroll-to-top');
+const topAnchor = document.querySelector('#top-anchor');
+let topOffset = document.body.scrollTop;
+
+//window.onscroll = ;
+
+window.addEventListener('scroll', () => {
+    console.log(topOffset);
+    if(document.body.scrollTop > 300) {
+        toTop.classList.add('to-top-visible');
+    } else {
+        toTop.classList.remove('to-top-visible');
+    }
 });
